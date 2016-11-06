b4ccaab77faf83bf5c6384635793684499c737bb
andrew <Andrew.Kostousov@gmail.com>
2016-11-06 16:15:17 +0500

    + deploy-plugin script

---
+++ deploy-plugin.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = "Stop";
+
+$BuildOutputDir = ".\src\target"
+$TeamCityDataDir = "d:\temp\team-city"
+
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{    
+    throw "This script needs to be run As Admin"
+}
+
+Stop-Service TeamCity
+Stop-Service TCBuildAgent
+
+Copy-Item "$BuildOutputDir\octo-pnp.zip" "$TeamCityDataDir\plugins\octo-pnp.zip" -Force
+
+Start-Service TeamCity
+Start-Service TCBuildAgent
+
